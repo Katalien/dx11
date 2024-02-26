bb19a83fbafb11541e93a1cdbd0132cad5493ee7
KateZin <90635745+KateZin@users.noreply.github.com>
2024-02-26 09:39:46 +0300

    lab3

--- lab2_dx11/rendering.h
+++ lab2_dx11/rendering.h
@@ -6,13 +6,24 @@
 #include <directxmath.h>
 #include <string>
 #include "Vertex.hpp"
+#include "camera.h"
+#include "input.h"
 
 using namespace DirectX;
 
+struct WorldMatrixBuffer {
+    XMMATRIX mWorldMatrix;
+};
+
+struct SceneMatrixBuffer {
+    XMMATRIX mViewProjectionMatrix;
+};
 
 class Renderer {
 public:
-    bool InitDevice(HWND hWnd);
+
+    bool Init(HINSTANCE hInstance, HWND hWnd);
+    bool Frame();
     bool Resize(UINT width, UINT height);
     void Cleanup();
     bool Render();
@@ -20,10 +31,13 @@ public:
 private:
     HRESULT InitScene();
     HRESULT SetupBackBuffer();
+
+    void ManageUserInput();
     
     ID3D11Device* m_pDevice = nullptr;
     ID3D11DeviceContext* m_pContext = nullptr;
     IDXGISwapChain* m_pSwapChain = nullptr;
+
     ID3D11RenderTargetView* m_pBackBufferRTV = nullptr;
     UINT m_width = 0;
     UINT m_height = 0;
@@ -32,4 +46,11 @@ private:
     ID3D11InputLayout* m_pInputLayout = nullptr;
     ID3D11VertexShader* m_pVertexShader = nullptr;
     ID3D11PixelShader* m_pPixelShader = nullptr;
+
+    ID3D11Buffer* m_pWorldMatrixBuffer = nullptr;
+    ID3D11Buffer* m_pSceneMatrixBuffer = nullptr;
+    ID3D11RasterizerState* m_pRasterizerState = nullptr;
+
+    Camera* m_pCamera = nullptr;
+    Input* m_pInput = nullptr;
 };
