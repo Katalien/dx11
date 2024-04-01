a78c29c6159f219d2150683ae1690b9069d5872b
KateZin <90635745+KateZin@users.noreply.github.com>
2024-04-01 18:04:29 +0300

    add Light.h

---
+++ lab6/Light.h
@@ -0,0 +1,16 @@
+#define SCREEN_NEAR 0.01f
+#define SCREEN_FAR 100.0f
+#define MAX_CUBE 30
+#define MAX_LIGHT 60
+
+struct LIGHT {
+    float4 lightPos;
+    float4 lightColor;
+};
+
+cbuffer LightBuffer : register (b2) {
+    float4 cameraPos;
+    int4 lightParams;
+    LIGHT lights[MAX_LIGHT];
+    float4 ambientColor;
+};
