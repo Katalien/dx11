b17484d82e9b40104f4d5ac2e0c62b3b2d0bec17
KateZin <90635745+KateZin@users.noreply.github.com>
2024-04-12 12:22:08 +0300

    copy previous lab verion to new folder

---
+++ lab8/lab8/Frustum.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <directxmath.h>
+using namespace DirectX;
+
+class Frustum {
+public:
+	Frustum(float screenDepth) :
+		m_screenDepth(screenDepth) {};
+	// Function to initialize frustum class
+	void Init(float screenDepth) { m_screenDepth = screenDepth; };
+	// Release function
+	void Release() {};
+	// Function to build frustum
+	void ConstructFrustum(XMMATRIX viewMatrix, XMMATRIX projectionMatrix);
+
+	// Functions to check if rectengle is in frustum
+	bool CheckRectangle(float maxWidth, float maxHeight, float maxDepth, float minWidth, float minHeight, float minDepth);
+private:
+	float m_screenDepth;
+	float m_planes[6][4];
+};
---
+++ lab8/lab8/Input.cpp
@@ -0,0 +1,44 @@
+#include "input.h"
+#include <fstream>
+
+void Input::Init(HINSTANCE hinstance, HWND hwnd) {
+    float x = 0.0f;
+    float y = 0.0f;
+
+    prevCursor.x = static_cast<LONG>(x);
+    prevCursor.y = static_cast<LONG>(y);
+    curCursor.x = static_cast<LONG>(x);
+    curCursor.y = static_cast<LONG>(y);
+}
+
+
+XMFLOAT3 Input::ReadMouse() {
+    POINT cursor;
+
+    if (GetCursorPos(&cursor)) {
+        ScreenToClient(hwnd, &cursor);
+        if (cursor.x == curCursor.x && cursor.y == curCursor.y) {
+            return XMFLOAT3(0.0f, 0.0f, 0.0f);
+        }
+
+        prevCursor = curCursor;
+        curCursor = cursor;
+        GetKeyboardState(keyboardState_);
+
+       if (keyboardState_[VK_LBUTTON] & 0x80) {
+            return  XMFLOAT3(
+                (curCursor.x - prevCursor.x) / 100.0f,
+                (curCursor.y - prevCursor.y) / 100.0f,
+                0.0f);
+        }
+        if (keyboardState_[VK_UP] & 0x80) {
+            return  XMFLOAT3(0.0f, 0.0f, -1.0f);
+        }
+        if (keyboardState_[VK_DOWN] & 0x80) {
+            return  XMFLOAT3(0.0f, 0.0f, 1.0f);
+        }
+        else {
+            return XMFLOAT3(0.0f, 0.0f, 0.0f);
+        }
+    }
+}
